81a40e4021a720efbdd31be95dcc0c4d9a666fc6
adetkr <tkradm@yahoo.fr>
2022-07-19 22:11:42 +0200

    add relations and validations

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,5 @@
 class Shop < ApplicationRecord
+  has_many :shop_opening_hours, inverse_of: :shop, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
 end
--- app/models/shop_opening_hour.rb
+++ app/models/shop_opening_hour.rb
@@ -1,2 +1,17 @@
 class ShopOpeningHour < ApplicationRecord
+  belongs_to :shop
+
+  validates :day, :open_at, :close_at, presence: true
+  validates :break_starts_at, presence: true, if: :break_ends_at?
+  validates :break_ends_at, presence: true, if: :break_starts_at?
+
+  enum day: {
+    monday: 0,
+    tuesday: 1,
+    wednesday: 2,
+    thursday: 3,
+    friday: 4,
+    saturday: 5,
+    sunday: 6,
+  }
 end
