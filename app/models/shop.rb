228412a7a4ccee8b539c450cca6e669daf8f9c2a
adetkr <tkradm@yahoo.fr>
2022-07-20 10:24:52 +0200

    add nested attributes

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,7 @@
 class Shop < ApplicationRecord
   has_many :shop_opening_hours, inverse_of: :shop, dependent: :destroy
+  accepts_nested_attributes_for :shop_opening_hours, allow_destroy: true
+  validates_associated :shop_opening_hours
 
   validates :name, presence: true, uniqueness: true
 end
