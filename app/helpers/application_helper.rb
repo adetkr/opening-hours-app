32a54c0a06161d29169c58c432d905ed5f6ddd3f
adetkr <tkradm@yahoo.fr>
2022-07-20 13:56:22 +0200

    enhance shop opening hours show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,11 @@ module ApplicationHelper
   end
 
   def presenter(current_opening_hour)
-    "#{I18n.t("activerecord.attributes.shop_opening_hour.days.#{current_opening_hour.day}")} #{current_opening_hour.open_at.strftime('%Hh%M')}-#{current_opening_hour.break_starts_at.strftime('%Hh%M')} #{current_opening_hour.break_ends_at.strftime('%Hh%M')}-#{current_opening_hour.close_at.strftime('%Hh%M')}"
+    day = I18n.t("activerecord.attributes.shop_opening_hour.days.#{current_opening_hour.day}")
+    return  "#{day}  Fermé" if current_opening_hour.closed?
+
+    return "#{day}  #{current_opening_hour.open_at.strftime('%Hh%M')}-#{current_opening_hour.break_starts_at.strftime('%Hh%M')}, #{current_opening_hour.break_ends_at.strftime('%Hh%M')}-#{current_opening_hour.close_at.strftime('%Hh%M')}" if current_opening_hour.break_hours_filled?
+
+    "#{day}  #{current_opening_hour.open_at.strftime('%Hh%M')}-#{current_opening_hour.close_at.strftime('%Hh%M')}"
   end
 end
