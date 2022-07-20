bfb9d40d0265c82c09fa611ba33cec5f9935b329
adetkr <tkradm@yahoo.fr>
2022-07-20 10:23:51 +0200

    init controller actions

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,4 +2,38 @@ class ShopsController < ApplicationController
   def index
     @shops = Shop.all
   end
+
+  def show
+    @shop = Shop.find(params[:id])
+  end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to shop_path(@shop)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @shop = Shop.find(params[:id])
+  end
+
+  def update
+    @shop = Shop.find(params[:id])
+    if @shop.update(shop_params)
+      redirect_to shop_path(@shop)
+    else
+      render :edit
+    end
+  end
+
+  def shop_params
+    params.require(:shop).permit(:name, shop_opening_hours_attributes: [:day, :open_at, :break_starts_at, :break_ends_at, :close_at, :_destroy])
+  end
 end
