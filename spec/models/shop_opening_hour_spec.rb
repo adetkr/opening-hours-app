3a7da0e21aacb73861b139b132c8246cd481ab85
adetkr <tkradm@yahoo.fr>
2022-07-21 09:37:42 +0200

    use absolute dates instead of relative ones in specs

--- spec/models/shop_opening_hour_spec.rb
+++ spec/models/shop_opening_hour_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ShopOpeningHour, type: :model do
-  describe 'validations' do
-    let(:shop) { Shop.create(name: 'name')}
+  let(:shop) { Shop.create(name: 'name')}
 
+  describe 'validations' do
     it { should validate_presence_of(:day) }
     it { should validate_presence_of(:open_at) }
     it { should validate_presence_of(:close_at) }
 
     context 'when break_ends_at is present' do
       let(:shop_opening_hour) do
-        ShopOpeningHour.create(shop: shop, day: :monday, open_at: 10.hours.ago, close_at: 1.hour.ago, break_ends_at: 7.hours.ago)
+        ShopOpeningHour.create(shop: shop, day: :monday, open_at: DateTime.new(1999, 6, 30, 8, 30), close_at: DateTime.new(1999, 6, 30, 12, 30), break_ends_at: DateTime.new(1999, 6, 30, 9, 30))
       end
       it 'should validates presence of break_starts_at' do
         expect(shop_opening_hour).not_to be_valid
@@ -19,7 +19,7 @@ RSpec.describe ShopOpeningHour, type: :model do
 
     context 'when break_starts_at is present' do
       let(:shop_opening_hour) do
-        ShopOpeningHour.create(shop: shop, day: :monday, open_at: 10.hours.ago, close_at: 1.hour.ago, break_starts_at: 7.hours.ago)
+        ShopOpeningHour.create(shop: shop, day: :monday, open_at: DateTime.new(1999, 6, 30, 8, 30), close_at: DateTime.new(1999, 6, 30, 12, 30), break_starts_at: DateTime.new(1999, 6, 30, 9, 30))
       end
       it 'should validates presence of break_ends_at' do
         expect(shop_opening_hour).not_to be_valid
@@ -28,7 +28,7 @@ RSpec.describe ShopOpeningHour, type: :model do
 
     context 'when open_at is greater than close_at' do
       let(:shop_opening_hour) do
-        ShopOpeningHour.create(shop: shop, day: :monday, open_at: 1.hours.ago, close_at: 10.hour.ago)
+        ShopOpeningHour.create(shop: shop, day: :monday, open_at: DateTime.new(1999, 6, 30, 12, 30), close_at: DateTime.new(1999, 6, 30, 8, 30))
       end
       it 'should validates close_at coherence' do
         expect(shop_opening_hour.errors.full_messages.to_sentence).to eq("Close at L'horaire de fermeture du magasin doit être après l'horaire d'ouverture du magasin")
@@ -37,7 +37,7 @@ RSpec.describe ShopOpeningHour, type: :model do
 
     context 'when break_ends_at is greater than close_at' do
       let(:shop_opening_hour) do
-        ShopOpeningHour.create(shop: shop, day: :monday, open_at: 10.hours.ago, close_at: 2.hour.ago, break_starts_at: 8.hours.ago, break_ends_at: 1.hours.ago)
+        ShopOpeningHour.create(shop: shop, day: :monday, open_at: DateTime.new(1999, 6, 30, 8, 30), close_at: DateTime.new(1999, 6, 30, 12, 30), break_starts_at: DateTime.new(1999, 6, 30, 9, 30), break_ends_at: DateTime.new(1999, 6, 30, 15, 30))
       end
       it 'should validates close_at coherence' do
         expect(shop_opening_hour.errors.full_messages.to_sentence).to eq("Close at L'horaire de fermeture du magasin doit être après l'horaire de la fin de pause")
@@ -46,7 +46,7 @@ RSpec.describe ShopOpeningHour, type: :model do
 
     context 'when break_starts_at is greater than break_ends_at' do
       let(:shop_opening_hour) do
-        ShopOpeningHour.create(shop: shop, day: :monday, open_at: 10.hours.ago, close_at: 1.hour.ago, break_starts_at: 4.hours.ago, break_ends_at: 5.hours.ago)
+        ShopOpeningHour.create(shop: shop, day: :monday, open_at: DateTime.new(1999, 6, 30, 8, 30), close_at: DateTime.new(1999, 6, 30, 12, 30), break_starts_at: DateTime.new(1999, 6, 30, 10, 30), break_ends_at: DateTime.new(1999, 6, 30, 9, 30))
       end
       it 'should validates break_ends_at coherence' do
         expect(shop_opening_hour.errors.full_messages.to_sentence).to eq("Break ends at L'horaire de la fin de pause doit être après l'horaire du début de la pause")
@@ -55,7 +55,7 @@ RSpec.describe ShopOpeningHour, type: :model do
 
     context 'when open_at is greater than break_starts_at' do
       let(:shop_opening_hour) do
-        ShopOpeningHour.create(shop: shop, day: :monday, open_at: 6.hours.ago, close_at: 1.hour.ago, break_starts_at: 7.hours.ago, break_ends_at: 4.hours.ago)
+        ShopOpeningHour.create(shop: shop, day: :monday, open_at: DateTime.new(1999, 6, 30, 8, 30), close_at: DateTime.new(1999, 6, 30, 12, 30), break_starts_at: DateTime.new(1999, 6, 30, 7, 30), break_ends_at: DateTime.new(1999, 6, 30, 11, 30))
       end
       it 'should validates break_starts_at coherence' do
         expect(shop_opening_hour.errors.full_messages.to_sentence).to eq("Break starts at L'horaire du début de la pause doit être après l'horaire d'ouverture du magasin")
@@ -70,7 +70,7 @@ RSpec.describe ShopOpeningHour, type: :model do
   describe '#break_hours_filled?' do
     context 'when break hours are not filled' do
       let(:shop_opening_hour) do
-        ShopOpeningHour.create(shop: shop, day: :monday, open_at: 10.hours.ago, close_at: 1.hour.ago, break_ends_at: 7.hours.ago)
+        ShopOpeningHour.create(shop: shop, day: :monday, open_at: DateTime.new(1999, 6, 30, 8, 30), close_at: DateTime.new(1999, 6, 30, 12, 30), break_ends_at: DateTime.new(1999, 6, 30, 11, 30))
       end
 
       it 'returns false' do
@@ -80,7 +80,7 @@ RSpec.describe ShopOpeningHour, type: :model do
 
     context 'when break hours are filled' do
       let(:shop_opening_hour) do
-        ShopOpeningHour.create(shop: shop, day: :monday, open_at: 10.hours.ago, close_at: 1.hour.ago, break_starts_at: 6.hours.ago, break_ends_at: 7.hours.ago)
+        ShopOpeningHour.create(shop: shop, day: :monday, open_at: DateTime.new(1999, 6, 30, 8, 30), close_at: DateTime.new(1999, 6, 30, 12, 30), break_starts_at: DateTime.new(1999, 6, 30, 11, 00), break_ends_at: DateTime.new(1999, 6, 30, 11, 30))
       end
 
       it 'returns true' do
